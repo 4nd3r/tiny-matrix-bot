1be0da65ad8be030c27ec2e4625940374e689e68
Ander Punnar <ander@kvlt.ee>
2016-12-21 19:12:15 +0200

    style

--- tiny-matrix-bot.py
+++ tiny-matrix-bot.py
@@ -10,26 +10,26 @@ from subprocess import check_output
 from matrix_client.client import MatrixClient
 
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding("utf-8")
 
 logging.basicConfig(level=logging.WARNING)
 
 config = ConfigParser.ConfigParser()
 config.read("tiny-matrix-bot.cfg")
 
-client = MatrixClient( config.get("tiny-matrix-bot", 'host') )
+client = MatrixClient(config.get("tiny-matrix-bot", "host"))
 
 token = client.login_with_password(
-    username=config.get("tiny-matrix-bot", 'user'),
-    password=config.get("tiny-matrix-bot", 'pass'))
+    username=config.get("tiny-matrix-bot", "user"),
+    password=config.get("tiny-matrix-bot", "pass"))
 
 def on_event(event):
-    if event['type'] == "m.room.message":
+    if event["type"] == "m.room.message":
         print("{0} {1} {2} {3}".format(
-            event['content']['msgtype'],
-            event['room_id'],
-            event['sender'],
-            event['content']['body']))
+            event["content"]["msgtype"],
+            event["room_id"],
+            event["sender"],
+            event["content"]["body"]))
 
 def on_invite(room_id, state):
     print("invite {0}".format(room_id))
@@ -41,18 +41,18 @@ def join_room(room_id):
     print("join {0}".format(room_id))
 
 def on_room_event(room, event):
-    if event['type'] == "m.room.message":
-        if event['content']['msgtype'] == "m.text":
+    if event["type"] == "m.room.message":
+        if event["content"]["msgtype"] == "m.text":
             m_text(room, event)
 
 def m_text(room, event):
-    body = event['content']['body']
+    body = event["content"]["body"]
     if not body.startswith("!"):
         return
     s = body.split()
     cmd = s[0][1:]
     s.pop(0)
-    args = ' '.join(s)
+    args = " ".join(s)
     script = os.path.abspath(cmd)
     if not os.path.isfile(script):
         print "ERROR: file `{0}' not found".format(script)
@@ -60,7 +60,7 @@ def m_text(room, event):
     if not stat.S_IXUSR & os.stat(script)[stat.ST_MODE]:
         print "ERROR: file `{0}' not executable".format(script)
         return
-    output = check_output( [ script, args ] ).strip()
+    output = check_output([script, args]).strip()
     for line in output.splitlines():
         sleep(0.5)
         room.send_notice(line)
