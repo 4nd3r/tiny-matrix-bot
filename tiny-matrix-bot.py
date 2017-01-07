aaca9fd69639f8e125f12f01f132f4dd482a8f73
Ander Punnar <ander@kvlt.ee>
2017-01-07 18:01:07 +0200

    control

--- tiny-matrix-bot.py
+++ tiny-matrix-bot.py
@@ -121,6 +121,11 @@ client.add_leave_listener(on_leave)
 
 while True:
     try:
-        raw_input()
+        input = raw_input()
+        if input == "rooms":
+            for room_id in client.get_rooms():
+                print(room_id)
+        if input.strip().startswith("part "):
+            client.get_rooms()[input[5:]].leave()
     except (EOFError, KeyboardInterrupt):
         exit()
